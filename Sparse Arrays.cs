ea2255e3953a0bbf74d52f092304894868b199ac
Rafał <44783572+rniedzielak@users.noreply.github.com>
2018-12-27 12:40:23 +0100

    Create Sparse Arrays.cs

---
+++ Sparse Arrays.cs
@@ -0,0 +1,62 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the matchingStrings function below.
+    static int[] matchingStrings(string[] strings, string[] queries)
+    {
+        int[] results = new int[queries.Length];
+        for(int i = 0; i < queries.Length; i++)
+        {
+            int counter = 0;
+            for(int j = 0; j < strings.Length;j++)
+            {
+                if(queries[i] == strings[j])
+                    counter++;
+            }
+            results[i] = counter;
+        }
+        return results;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int stringsCount = Convert.ToInt32(Console.ReadLine());
+
+        string[] strings = new string [stringsCount];
+
+        for (int i = 0; i < stringsCount; i++) {
+            string stringsItem = Console.ReadLine();
+            strings[i] = stringsItem;
+        }
+
+        int queriesCount = Convert.ToInt32(Console.ReadLine());
+
+        string[] queries = new string [queriesCount];
+
+        for (int i = 0; i < queriesCount; i++) {
+            string queriesItem = Console.ReadLine();
+            queries[i] = queriesItem;
+        }
+
+        int[] res = matchingStrings(strings, queries);
+
+        textWriter.WriteLine(string.Join("\n", res));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
